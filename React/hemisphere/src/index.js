0aed6d3e6b7b8e7b5e156828f91c257513bf42e8
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-03-26 18:30:30 +0300

    conditional rendering

--- React/hemisphere/src/index.js
+++ React/hemisphere/src/index.js
@@ -12,9 +12,17 @@ determining location using geolocation API*/
 timeline of application inside the browser
 -JS file loaded up by the broser 
 -App components gets created
+-constructor function gets called
+-'this.state' property assigned the state object
 - we call geolocation service
+-React calls the component render method
 -App gets rendered to page as HTML
--We get result of geolocation*/
+-We get result of geolocation
+-we update our state object with a 'this.setState'
+-React calls 'render'method a second time
+-Render method returns some JSX
+-React takes that JSX and updates content on the screen
+*/
 
 /*
 rules of class-based components
@@ -48,25 +56,59 @@ state rules
 -State is a JS object that contains data relevant to a component
 -Updating state on a component causes the component to rerender
 -State must be initialized when a component is created
--State can only be updated using the function 'setState'
+-State can only be updated using the function 'setState' as opposed to doing a direct assignment
  */
 
+/*
+
+lifecycle methods
+-constructor - state initialization - initial data loading
+-render-just return JSX
+-componentDidMount - good place to data loading
+-componentDidUpdate- good place to load more data loading when state/props change
+-componentWillUnmount- good place to clean up
+*/
+
 class App extends React.Component {
-    //function that is called first
+    //function that is called first any time an instance is created
+    // eslint-disable-next-line no-useless-constructor
     // constructor(props){
-    //  //
-    //   super(props)
+    //   //super is a reference to the parents constructor function
+    //    super(props)
+
+    //    this.state = { latitude:null , errorMessage: ''}
+       
+       
     // }
+
+    state = {latitude:null, errorMessage:''}
+
+    componentDidMount(){
+      window.navigator.geolocation.getCurrentPosition(
+        //success callback
+        (position) => {
+          this.setState({latitude:position.coords.latitude})
+        },
+        (error) => {
+          this.setState({errorMessage: error.message})
+        }
+      );
+    }
+
+    //render method is required for every react component we create
+
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      //success callback
-      (position) => console.log(position),
-      (error) => console.log(error)
-    );
-    return (
-        <div>You are in the northern hemisphere</div>
-    )
-    
+    //conditional rendering 
+    if(this.state.errorMessage && !this.state.latitude){
+      return <div>{this.state.errorMessage}</div>
+    }
+
+    if(!this.state.errorMessage && this.state.latitude ){
+      return <div>{this.state.latitude}</div>
+    }
+    else{
+      return <div>Loading...</div>
+    }
   }
 }
 
