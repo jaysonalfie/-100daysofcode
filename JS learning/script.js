146a269ce50b91d3d37cafac11efc69311bba05d
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-06-24 18:03:54 +0300

    js learning

---
+++ JS learning/script.js
@@ -0,0 +1,46 @@
+console.log("hello world")
+
+//variables
+name = 'Jayson Alfie'
+console.log(name);
+
+sentence = 'What a great day to be alive as we strive to have the best life ever'
+console.log(sentence)
+
+//operators
+// fruit = prompt('What is you favorite fruit?')
+// console.log(fruit)
+
+
+meat = Number(prompt('How much does the meat go for?'))
+tipPercentage = Number(prompt('tip % ?') /100)
+tipAmount = meat * tipPercentage
+console.log('tip amount', tipAmount)
+total = meat + tipAmount
+console.log( 'total', total)
+//alert(tipAmount)
+
+//user input
+//prompt() is a functionality that enables you to get inputs from the user through the browser
+//Use of Number function to convert strings to numbers
+
+/**
+ * Datatypes (strings, numbers,arrays, objects, boolean )
+ * 
+ */
+
+/**
+ * Math operators
+ * 
+ * -Multiplication
+ * -Division
+ * -Addition
+ * -Subtraction
+ * Exponents **
+ * Module %
+  
+*/
+
+/**
+  Math Methods
+ */
