8d15e32992a680a15bbbb7653e5abf0ac2f846a7
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-07-04 19:38:49 +0300

    learnt about components and props

--- DOM/script.js
+++ DOM/script.js
@@ -14,7 +14,50 @@
 // //place the H! element inside the div
 // app.append(header)
 
+
+
+//creating components which are functions in react
+
+function Header({title}){
+    console.log(title);
+    return(
+     <>
+           {/* // title is a variable */}
+        <h1>{title}</h1>
+        <h1>{`cool ${title}`}</h1>
+        <h1>{title ? title : 'Default Title'}</h1>
+     {/* <div>
+         
+         <h1>Develop. Preview. Ship</h1>
+        
+     </div> */}
+     <Nav titleName='Alpha'/>
+     </>
+    )
+}
+
+//nesting components together
+function HomePage (){
+    const names = ['Ada lovelace', 'kendi Beckham', 'Larry Wells']
+    {/* nesting the header inside the Homepage component*/}
+    return<div>
+        <Header title="react"/>
+        <Header title ="a new beginning"/>
+        <ul>
+            {names.map((name)=> (
+                <li key={name}>{name}</li>
+            ))}
+        </ul>
+    </div>
+}
+
+//nav to add to header
+function Nav({titleName}){
+
+    return(<p>{titleName}</p>)
+}
+
 //adding react DOM
 const domNode= document.getElementById("app");
 const root = ReactDOM.createRoot(domNode);
-root.render(<h1>Develop. Preview. Ship.</h1>);
+root.render(<HomePage/>);
